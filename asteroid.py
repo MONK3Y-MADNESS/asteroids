0c6869580eb961b1a125918826d72c16d04e3433
MONK3Y-MADNESS <btimbrell14@gmail.com>
2025-01-31 19:21:39 +1000

    Asteroids now break into smaller asteroids when shot.

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+from constants import ASTEROID_MIN_RADIUS
 from circleshape import CircleShape
 
 class Asteroid(CircleShape):
@@ -10,3 +12,19 @@ class Asteroid(CircleShape):
 
 	def update(self, dt):
 		self.position += self.velocity * dt
+
+	def split(self):
+		self.kill()
+		new_radius = self.radius - ASTEROID_MIN_RADIUS
+		if self.radius <= ASTEROID_MIN_RADIUS:
+			return
+
+		random_angle = random.uniform(20, 50)
+
+		new_1 = self.velocity.rotate(random_angle)
+		asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+		asteroid.velocity = new_1 * 1.2
+
+		new_2 = self.velocity.rotate(-random_angle)
+		asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+		asteroid.velocity = new_2 * 1.2
--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def main():
 			for shot in shots:
 				if asteroid.collisions(shot):
 					shot.kill()
-					asteroid.kill()
+					asteroid.split()
 
 		screen.fill("black")
 		for obj in drawable:
